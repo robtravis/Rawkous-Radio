f292ea156093c3888dcb1517a0ecae52937ce18b
Rawkous Radio <radio@rawkous.com>
2020-08-24 11:10:34 -0400

    Intial Upload

---
+++ SwiftRadio/SwiftRadio-Settings.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftRadio-Settings.swift
+//  Swift Radio
+//
+//  Created by Matthew Fecher on 7/2/15.
+//  Copyright (c) 2015 MatthewFecher.com. All rights reserved.
+//
+
+import Foundation
+
+//**************************************
+// GENERAL SETTINGS
+//**************************************
+
+// Display Comments
+let kDebugLog = true
+
+// Main Screen title
+let mainScreentitle = "RAWKOUS RADIO"
+
+// Station Data
+
+let stationName = "RAWKOUS RADIO"
+let stationStreamURL = "https://streaming.radio.co/s1bd76cba7/listen"
+
+// The file should be present in the asset catalog (Images.xcassets)
+let stationImageURL = "station-rawkous.png"
+let stationDesc = "(((TUNE IN TOGETHER)))"
+let stationlongDesc = "Created by friends to keep connected. Powered by listners who love radio."
+
+// Set this to "true" to hide the AirPlay button
+let hideAirPlayButton = false
